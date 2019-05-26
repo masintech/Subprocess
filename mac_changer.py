e6f8788dd9dc49ee35c2d9e4526768ebf952dcf8
masintech <masintec@gmail.com>
2019-05-26 03:22:32 -0400

    add function

--- mac_changer.py
+++ mac_changer.py
@@ -9,6 +9,13 @@ def mac_changer(interface, mac_addr):
     subprocess.call(["sudo","ifconfig",interface,"hw","ether",mac_addr])
     # enable the interface
     subprocess.call(["sudo","ifconfig",interface,"up"])
+def get_current_mac (interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result :
+        return mac_address_search_result.group(0)
+    else:
+        print()
 
 if __name__=="__main__" :
 
